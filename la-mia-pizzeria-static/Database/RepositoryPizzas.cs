cb94ef73c7fdda297391a4b6f394bf4236a427f5
KatiaLosignore <katialos.kl@gmail.com>
2023-10-09 15:28:14 +0200

    implemented interface IRepositoryPizzas in class RepositoryPizzas

--- la-mia-pizzeria-static/Database/RepositoryPizzas.cs
+++ la-mia-pizzeria-static/Database/RepositoryPizzas.cs
@@ -1,4 +1,5 @@
 ﻿using la_mia_pizzeria_static.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace la_mia_pizzeria_static.Database
 {
@@ -14,34 +15,93 @@ namespace la_mia_pizzeria_static.Database
         }
 
         // implemento i Metodi creati nell' Interfaccia
-        public bool AddPizza(Pizza pizzaToAdd)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool DeletePizza(int id)
-        {
-            throw new NotImplementedException();
-        }
-
         public Pizza GetPizzaById(int id)
         {
-            throw new NotImplementedException();
+            Pizza? pizza = _db.Pizzas.Where(pizza => pizza.Id == id)
+                               .Include(pizza => pizza.Ingredients).Include(pizza => pizza.Category).FirstOrDefault();
+
+            if (pizza != null)
+            {
+                return pizza;
+            }
+            else
+            {
+                throw new Exception("La pizza non è stata trovata!");
+            }
         }
 
         public List<Pizza> GetPizzas()
         {
-            throw new NotImplementedException();
+            List<Pizza> pizzas = _db.Pizzas.Include(pizza => pizza.Ingredients).ToList();
+            return pizzas;
         }
 
         public List<Pizza> GetPizzasByTitle(string title)
         {
-            throw new NotImplementedException();
+            List<Pizza> foundedPizzsd =
+              _db.Pizzas.Where(pizza => pizza.Name.ToLower().Contains(title.ToLower())).ToList();
+
+            return foundedPizzsd;
+        }
+
+        public bool AddPizza(Pizza pizzaToAdd)
+        {
+            try
+            {
+                _db.Pizzas.Add(pizzaToAdd);
+                _db.SaveChanges();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public bool ModifyPizza(int id, Pizza updatedPizza)
         {
-            throw new NotImplementedException();
+            Pizza? pizzaToUpdate = _db.Pizzas.Where(pizza => pizza.Id == id).Include(pizza => pizza.Ingredients).FirstOrDefault();
+
+            if (pizzaToUpdate != null)
+            {
+                pizzaToUpdate.Ingredients.Clear();
+
+                pizzaToUpdate.Name = updatedPizza.Name;
+                pizzaToUpdate.Description = updatedPizza.Description;
+                pizzaToUpdate.Price = updatedPizza.Price;
+                pizzaToUpdate.Image = updatedPizza.Image;
+                pizzaToUpdate.CategoryId = updatedPizza.CategoryId;
+
+
+                _db.SaveChanges();
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
+        public bool DeletePizza(int id)
+        {
+
+            Pizza? pizzaToDelete = _db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+            if (pizzaToDelete == null)
+            {
+                return false;
+            }
+
+            _db.Pizzas.Remove(pizzaToDelete);
+            _db.SaveChanges();
+
+            return true;
+
+        }
+
+
     }
+
+    
 }
